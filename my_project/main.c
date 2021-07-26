3c38851c051575be0bb36a45b3c9d19481b1dfbe
Jabaca3 <jojobaca123@gmail.com>
2021-07-26 17:27:28 -0600

    updating files

--- my_project/main.c
+++ my_project/main.c
@@ -4,11 +4,14 @@
 #include "led.h"
 #include "switches.h"
 #include "buzzer.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
 
 void main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   led_init();
   switch_init();
+  lcd_init();
   buzzer_init();
   buzzer_set_period(0);
   enableWDTInterrupts();	/* enable periodic interrupt */
