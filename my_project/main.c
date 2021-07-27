52be1eeb29f602cdb127a91542dbdd3727e1d11c
Jabaca3 <jojobaca123@gmail.com>
2021-07-26 19:51:14 -0600

    updating files

--- my_project/main.c
+++ my_project/main.c
@@ -8,20 +8,18 @@
 #include "../lcdLib/lcddraw.h"
 
 short redrawScreen = 1;
-u_int fontFgColor = COLOR_GREEN;
 
 void wdt_c_handler(){
-  clearScreen(COLOR_WHITE);
+  
 }
 
 void main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-  led_init();
+  //led_init();
   switch_init();
   lcd_init();
   buzzer_init();
   buzzer_set_period(0);
-  enableWDTInterrupts();	/* enable periodic interrupt */
-
+  enableWDTInterrupts();	/* enable periodic interrupt */  
   or_sr(0x18);		/* CPU off, GIE on */
 }
